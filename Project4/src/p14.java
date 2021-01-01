4f4331b439815a73147b931597efd90f88bbe6ba
mkwkw <mkkw2047@gmail.com>
2021-01-01 21:18:38 +0900

    EOF

---
+++ Project4/src/p14.java
@@ -0,0 +1,26 @@
+//백준 10951
+//EOF
+//Scanner - hasNextInt()사용
+//BufferedReader- null을 반환할 때 종료
+import java.util.*;
+public class p14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			
+			System.out.println(n1+n2);
+			
+			
+		}
+		
+		
+	
+	}
+
+}
